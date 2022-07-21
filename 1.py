8c67b68bd6ec7be2853f5ae9016cc8e0d2c895eb
Ilian Nikolov Kossev <i.kossev@abv.bg>
2022-07-21 06:49:27 +0300

    Exercises

---
+++ 1.py
@@ -0,0 +1,42 @@
+# file = open('demo.py')
+#
+# b = 1536
+#
+# while True:
+#     a = file.read(3)
+#     c = file.readline(3)
+#     print(a)
+#     print(c)
+#     if not a:
+#         break
+
+import random, time, sys
+
+#
+# number = random.randint(0, 50)
+# print('Здравей, как се казваш!')
+# name = input()
+# name = name[0].upper() + name[1:].lower()
+# print(f'Здравей {name}, искаш ли да познаеш кое число съм намислил?')
+# reply = input()
+# if reply == 'да':
+#     print('Моля въведи число от 0 до 50')
+# else:
+#     print('Тогава ще играем друг път! Чао!')
+#     sys.exit()
+# while True:
+#     num = int(input())
+#     print('числото е прието за обработка ...')
+#     time.sleep(2)
+#
+#     if num > number:
+#         print('Числото е твърде голямо. Опитай пак!')
+#     elif num < number:
+#         print('Числото е твърде малко. Опитай пак!')
+#     else:
+#         print('Бинго! Ти успя!')
+#         break
+#
+
+print(range(4))
+print(list(range(0, 100, 2)))
---
+++ demo.py
@@ -0,0 +1,4 @@
+x = [5, 8, 0, 4, 6]
+for el in x:
+    if el == 0:
+        x.replace
